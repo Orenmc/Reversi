7ad3d026340cf271e69955d106d5b1c979859f88
SnirHazan <snirhazan30@gmail.com>
2017-11-29 23:48:54 +0200

    test to board

--- last_update/test/test_Board.cpp
+++ last_update/test/test_Board.cpp
@@ -7,6 +7,7 @@
 
 #include "gtest/gtest.h"
 #include "Board.h"
+#include "GameLogic.h"
 
 TEST(Test_Board,Test_init_Board){
 	Board* b = new Board(8);
@@ -36,5 +37,23 @@ TEST(Test_Board,Test_Set_In_Board){
 	b->set_matrix(7,7,'O');
 	EXPECT_EQ(b->get_cell(2,2),'X');
 	EXPECT_EQ(b->get_cell(7,7),'O');
+	delete b;
+}
+
+TEST(Test_Board,Test_Point_To_Point){
+	Board* b = new Board(8);
+	b->set_matrix(2,2,'X');
+	b->set_matrix(7,7,'X');
+	Player* p = new Player("Player",'X');
+	Point p1(2,2);
+	Point p2(7,7);
+	GameLogic* g = new GameLogic(1,8);
+	g->change_board_point_to_point(b,p1,p2,p);
+	b->print_matrix();
+
+	for(int i,j = 1 ; i < b->get_size() ; i++,j++){
+				EXPECT_EQ(b->get_cell(i,j),'X');
+			}
+
 	delete b;
 }
