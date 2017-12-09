92ba70ab1ae27b40ae2c021425d796581b94207b
SnirHazan <snirhazan30@gmail.com>
2017-12-09 18:03:22 +0200

    "server"

---
+++ last update snir/Reversi_Server/Server.h
@@ -0,0 +1,26 @@
+/*
+ * Server.h
+ *
+ *  Created on: Dec 4, 2017
+ *      Author: snirhazan
+ */
+
+#ifndef SERVER_H_
+#define SERVER_H_
+
+class Server{
+public:
+	Server(int port);
+	void start();
+	void stop();
+
+private:
+	int port;
+	int serverSocket;
+
+	void handleClients(int clientSocket1,int clientSocket2);
+};
+
+
+
+#endif /* SERVER_H_ */
